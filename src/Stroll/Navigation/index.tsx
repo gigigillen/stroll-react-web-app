6cfed38b4bf05f8a50573c8596bdb69fd2007fd5
Gigi <gillen.g@northeastern.edu>
2024-08-14 16:10:29 -0400

    catching up so i can switch to homepage

---
+++ src/Stroll/Navigation/index.tsx
@@ -0,0 +1,9 @@
+import { Link, useLocation } from "react-router-dom";
+
+export default function StrollNavigation() {
+    const { pathname } = useLocation();
+    return(
+        <div>
+        </div>
+    );
+}
